d13215ecd08b71ee0eeb9eb7a37b5c3798228e18
Matteo_Pizzinato <matteop.pizzinato@live.com>
2019-02-08 15:04:50 +0100

    Sto cercando di sistemare la questione dell'arrotondamento delle ore con una funzione dedicata che prende i due numenri dopo la virgola nel risultato, li verifica e riassegna al conta ore

--- Functions.js
+++ Functions.js
@@ -17,10 +17,48 @@ function create_input_hours() {  // funzione che permette di scrivere dentro una
     x.setAttribute("value", "ciao");
     document.body.appendChild(x);
 }
-
+/*
 function sum_num1_plus_10(work_hours){
     work_hours = parseInt(document.getElementById("input_num").value);
     var num2 = 10;
     var result = work_hours + num2;
     alert(result);
+}
+*/
+function lavorazione_lenti(work_hours)
+{
+    work_hours = parseInt(document.getElementById("input_num").value);
+    // creo le variabili che determinano le temmpistiche per ogni fase di lavorazione
+    var lavorazione_1 = 8;
+    var lavorazione_2 = 2;
+    var lavorazione_3 = 6;
+    var lavorazione_4 = 37;
+    var lavorazione_5 = 22;
+    var lavorazione_6 = 15;
+    var lavorazione_7 = 10;
+    var result_1;
+    var result_2;
+    var result_3;
+    var result_4;
+    var result_5;
+    var result_6;
+    var result_7;
+    // adesso calcolo il risultato delle percentuali 
+    var result_1 = (work_hours * lavorazione_1) / 100;
+    var result_2 = (work_hours * lavorazione_1) / 100;
+    var result_3 = (work_hours * lavorazione_1) / 100;
+    var result_4 = (work_hours * lavorazione_1) / 100;
+    var result_5 = (work_hours * lavorazione_1) / 100;
+    var result_6 = (work_hours * lavorazione_1) / 100;
+    var result_7 = (work_hours * lavorazione_1) / 100;
+    // mostro i risultati 
+    alert(result_1);
+
+
+
+
+
+
+
+
 }
