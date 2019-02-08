58823326393c4930600efd232c76dcb22a5fce47
Matteo_Pizzinato <matteop.pizzinato@live.com>
2019-02-08 10:25:19 +0100

    Prova per far comparire spazio dove inserire ore

---
+++ Functions.js
@@ -0,0 +1,8 @@
+function Show_Hide_Hours_Input() {
+    var x = document.getElementById("Hours_Input");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
